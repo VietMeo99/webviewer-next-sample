2a5e46216d8224967b0f2d82f174af312d30a39d
VuViet <vuhoangviet160799@gmail.com>
2024-03-16 09:55:09 +0700

    integrate pdf webview

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from "react";
+
+export default function HomePage() {
+  const viewer = useRef(null);
+
+  useEffect(() => {
+    import("@pdftron/webviewer").then(() => {
+      WebViewer(
+        {
+          path: "/webviewer/lib",
+          initialDoc: "/files/VUHOANGVIET_Resume.pdf",
+          licenseKey: "your_license_key", // sign up to get a free trial key at https://dev.apryse.com
+        },
+        viewer.current
+      ).then((instance) => {
+        const { docViewer } = instance;
+        // you can now call WebViewer APIs here...
+      });
+    });
+  }, []);
+
+  return (
+    <div className="MyComponent">
+      <div className="webviewer" ref={viewer} style={{ height: "100vh" }}></div>
+    </div>
+  );
+}
